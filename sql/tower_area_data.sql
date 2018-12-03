1cc9f7d468f3b49466433500e69c63bd0a875f6d
miaocloud <lshy19931224@gmail.com>
2018-12-03 22:59:38 +0800

    init

--- sql/tower_area_data.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tower_area_data` (`tower_area_id` INTEGER NOT NULL,`max_floor_num` INTEGER NOT NULL,`area_bg` INTEGER NOT NULL,`tower_bgm` TEXT NOT NULL,PRIMARY KEY(`tower_area_id`));
-TRUNCATE TABLE tower_area_data;
-INSERT INTO tower_area_data VALUES (1,70,81000104,"bgm_M125");
-INSERT INTO tower_area_data VALUES (2,110,81000104,"bgm_M125");
-INSERT INTO tower_area_data VALUES (3,130,81000104,"bgm_M125");
--- sql/tower_schedule.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tower_schedule` (`tower_schedule_id` INTEGER NOT NULL,`max_tower_area_id` INTEGER NOT NULL,`opening_story_id` INTEGER NOT NULL,`count_start_time` TEXT NOT NULL,`recovery_disable_time` TEXT NOT NULL,`start_time` TEXT NOT NULL,`end_time` TEXT NOT NULL,PRIMARY KEY(`tower_schedule_id`));
-TRUNCATE TABLE tower_schedule;
-INSERT INTO tower_schedule VALUES (1001,1,7001000,"2018/09/17 14:59:59","2018/09/18 5:00:00","2018/09/13 12:00:00","2018/09/18 14:59:59");
-INSERT INTO tower_schedule VALUES (1002,2,7002000,"2018/10/17 14:59:59","2018/10/18 5:00:00","2018/10/13 12:00:00","2018/10/18 14:59:59");
-INSERT INTO tower_schedule VALUES (1003,3,7003000,"2018/11/18 14:59:59","2018/11/19 5:00:00","2018/11/14 12:00:00","2018/11/19 14:59:59");
