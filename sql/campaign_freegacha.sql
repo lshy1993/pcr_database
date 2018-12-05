d4d79baac12de9149aa2509cc7b0b16f5a4e0734
miaocloudbot <lshy19931224@gmail.com>
2018-12-05 14:51:55 +0800

    10005300

---
+++ sql/campaign_freegacha.sql
@@ -0,0 +1,4 @@
+DROP TABLE campaign_freegacha;
+CREATE TABLE IF NOT EXISTS `campaign_freegacha` (`id` INTEGER NOT NULL,`campaign_id` INTEGER NOT NULL,`freegacha_1` INTEGER NOT NULL,`freegacha_10` INTEGER NOT NULL,`start_time` TEXT NOT NULL,`end_time` TEXT NOT NULL,PRIMARY KEY(`id`));
+INSERT INTO `campaign_freegacha` VALUES (1,1,0,1,"2018/05/22 5:00:00","2018/06/01 4:59:59");
+INSERT INTO `campaign_freegacha` VALUES (2,2,0,1,"2018/08/21 5:00:00","2018/09/01 4:59:59");
---
+++ sql/hatsune_special_enemy.sql
@@ -0,0 +1,2 @@
+DROP TABLE hatsune_special_enemy;
+CREATE TABLE IF NOT EXISTS `hatsune_special_enemy` (`enemy_id` INTEGER NOT NULL,`event_id` INTEGER NOT NULL,`mode` INTEGER NOT NULL,`enemy_point` INTEGER NOT NULL,`initial_position` INTEGER NOT NULL,`order` INTEGER NOT NULL,PRIMARY KEY(`enemy_id`));
