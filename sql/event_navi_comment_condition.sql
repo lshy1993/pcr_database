e6b450e5b89e8aa183d8312152a367cce2b4925e
miaocloudbot <lshy19931224@gmail.com>
2018-12-05 14:34:53 +0800

    10005300

---
+++ sql/event_navi_comment_condition.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS `event_navi_comment_condition` (`comment_id` INTEGER NOT NULL,`condition_type_1` INTEGER NOT NULL,`condition_value_1` INTEGER NOT NULL,`condition_type_2` INTEGER NOT NULL,`condition_value_2` INTEGER NOT NULL,`condition_type_3` INTEGER NOT NULL,`condition_value_3` INTEGER NOT NULL,PRIMARY KEY(`comment_id`));
+TRUNCATE TABLE event_navi_comment_condition;
