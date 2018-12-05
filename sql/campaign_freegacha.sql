7b9e88c512d47ae06e8e8d34dde2113415e13159
miaocloudbot <lshy19931224@gmail.com>
2018-12-05 14:00:34 +0800

    10005300

--- sql/campaign_freegacha.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS `campaign_freegacha` (`id` INTEGER NOT NULL,`campaign_id` INTEGER NOT NULL,`freegacha_1` INTEGER NOT NULL,`freegacha_10` INTEGER NOT NULL,`start_time` TEXT NOT NULL,`end_time` TEXT NOT NULL,PRIMARY KEY(`id`));
-TRUNCATE TABLE campaign_freegacha;
-INSERT INTO campaign_freegacha VALUES (1,1,0,1,"2018/05/22 5:00:00","2018/06/01 4:59:59");
-INSERT INTO campaign_freegacha VALUES (2,2,0,1,"2018/08/21 5:00:00","2018/09/01 4:59:59");
--- sql/room_release_data.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `room_release_data` (`system_id` INTEGER NOT NULL,`story_id` INTEGER NOT NULL,`pre_story_id` INTEGER NOT NULL,PRIMARY KEY(`system_id`));
-TRUNCATE TABLE room_release_data;
-INSERT INTO room_release_data VALUES (601,4001001,0);
-INSERT INTO room_release_data VALUES (602,4001002,4001001);
-INSERT INTO room_release_data VALUES (603,4001003,4001002);
